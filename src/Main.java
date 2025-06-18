06c9ef91815260d07acf86849cfd0f0307cb197b
Rikard <rikardengstrom89@gmail.com>
2025-06-18 15:00:22 +0200

    tada

--- src/Main.java
+++ src/Main.java
@@ -14,5 +14,10 @@ public class Main {
 
         joe.retire();
         System.out.println("Joe's Pension check = $" + joe.collectPay());
+
+        HourlyEmployee mary = new HourlyEmployee("Mary", "05/05/1970", "03/03/2021", 15);
+        System.out.println(mary);
+        System.out.println("Mary's paycheck = $" + mary.collectPay());
+        System.out.println("Mary's Holiday pay = $" + mary.getDoublePay());
     }
 }
